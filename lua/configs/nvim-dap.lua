efa2d836a6a8ce17c2cf5e36d6c209d54b9f6b19
C4 Patino <c4patino@gmail.com>
2024-08-26 23:24:30 -0500

    Added gdb debugger to neovim configuration

--- lua/configs/nvim-dap.lua
+++ lua/configs/nvim-dap.lua
@@ -23,3 +23,45 @@ set('n', '<leader>dc', function() dap.continue() end, { desc = 'Debug Continue' 
 set('n', '<leader>do', function() dap.step_over() end, { desc = 'Debug Step over' });
 set('n', '<leader>di', function() dap.step_out() end, { desc = 'Debug Step into' });
 set('n', '<leader>dr', function() dap.repl.open() end, { desc = 'Debug Toggle repl' });
+
+dap.adapters.gdb = {
+    type = "executable",
+    command = "gdb",
+    args = { "--interpreter=dap", "--eval-command", "set print pretty on" }
+}
+
+dap.configurations.c = {
+    {
+        name = "Launch",
+        type = "gdb",
+        request = "launch",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = "${workspaceFolder}",
+        stopAtBeginningOfMainSubprogram = false,
+    },
+    {
+        name = "Select and attach to process",
+        type = "gdb",
+        request = "attach",
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        pid = function()
+            local name = vim.fn.input('Executable name (filter): ')
+            return require("dap.utils").pick_process({ filter = name })
+        end,
+        cwd = '${workspaceFolder}'
+    },
+    {
+        name = 'Attach to gdbserver :1234',
+        type = 'gdb',
+        request = 'attach',
+        target = 'localhost:1234',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}'
+    },
+}
